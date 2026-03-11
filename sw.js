4ffb504465bb3cf75d15d3e4b4ae5c4b7aa0f50c
KadeOrSomething <kade20413@gmail.com>
2026-03-10 21:42:33 -0500

    Widen page layouts to fill screen: remove/increase max-width constraints, add full-width friend profile, bump cache v9

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'showboat-v8';
+const CACHE_NAME = 'showboat-v9';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
-  '/css/style.css?v=6',
+  '/css/style.css?v=7',
   '/js/firebase-config.js',
   '/js/api.js',
   '/js/services.js',
